be98e6d8b0f6b3188f5b45e86ddbbac53d0dba20
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 09:12:31 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Hello
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in);
+System.out.println("Enter a String");
+String a=br.readLine();
+for(int i=0;i<=5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
